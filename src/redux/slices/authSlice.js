63c588006f91b41ea358e15f5feaf57fdf8f4b39
Peter Dumbari <dummytwiz@gmail.com>
2024-06-22 22:42:57 +0100

    fixing the final bug on display few books

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const BASE_URL = "https://ebook-library-taol.onrender.com/users";
+const BASE_URL = "http://localhost:3000/users";
 
 const initialState = {
   isAuthenticated: null,
